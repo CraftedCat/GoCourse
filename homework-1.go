f3c8e1ce46e688e7502dd723433bbc2c23e4ba7d
Troy <CraftedCatMail@gmail.com>
2019-04-22 21:25:22 +0300

    Printf

--- homework-1.go
+++ homework-1.go
@@ -11,7 +11,7 @@ func main() {
 	var kate2 float64
 	fmt.Println("Введите ваше кол-во рублей")
 	fmt.Scanln(&rub)
-	fmt.Println(currency(rub))
+	fmt.Printf("Кол-во долларов:%f \n", currency(rub))
 	fmt.Println("Введите длины катетов в сантиметрах через пробел:")
 	fmt.Scanln(&kate1, &kate2)
 	fmt.Printf("Площадь :%f кв. см", ploshad(kate1, kate2))
@@ -19,20 +19,11 @@ func main() {
 
 }
 
-func round(num float64) int {
-	return int(num + math.Copysign(0.5, num))
-}
-
-func toFixed(num float64, precision int) float64 {
-	output := math.Pow(10, float64(precision))
-	return float64(round(num*output)) / output
-}
-
 func currency(rub float64) float64 {
 	const curs = 65.5
 	var usd float64
 	usd = rub / curs
-	return toFixed(usd, 2)
+	return usd
 }
 
 func ploshad(kate1 float64, kate2 float64) float64 {
@@ -40,9 +31,9 @@ func ploshad(kate1 float64, kate2 float64) float64 {
 }
 
 func perimeter(kate1 float64, kate2 float64) float64 {
-	return toFixed(gipotenuza(kate1, kate2)+kate1+kate2, 2)
+	return gipotenuza(kate1, kate2) + kate1 + kate2
 }
 
 func gipotenuza(kate1, kate2 float64) float64 {
-	return toFixed(math.Sqrt(math.Pow(kate1, 2)+math.Pow(kate2, 2)), 2)
+	return math.Sqrt(math.Pow(kate1, 2) + math.Pow(kate2, 2))
 }
